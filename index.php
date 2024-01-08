c1d514ac3709309cbd92bba027d2123490504e27
Richard <prasomsoukrichard@gmail.com>
2024-01-08 12:03:59 +0100

    correction syntaxique

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=é, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
-    <title>Puissance4</title>
+    <title>Puissance 4</title>
   </head>
   <body>
     <table>
